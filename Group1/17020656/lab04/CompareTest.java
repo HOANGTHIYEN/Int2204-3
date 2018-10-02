d9fc19b6feff7e5a9808569da4ca63dcf4babd49
duongnt99 <duongnt99@users.noreply.github.com>
2018-10-02 23:58:32 +0700

    AddTest

---
+++ Group1/17020656/lab04/CompareTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compare;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Admin
+ */
+public class CompareTest {
+    @Test
+    public void testMax1()
+    {
+       assertEquals(20, Compare.max(10,20) );
+    }
+    @Test
+    public void testMax2()
+    {
+       assertEquals(50, Compare.max(10,50) );
+    }
+    @Test
+    public void testMax3()
+    {
+       assertEquals(-30,Compare.max(-70,-30));
+    }
+    @Test
+    public void testMax4()
+    {
+       assertEquals(0, Compare.max(-20, 0));
+    }
+    @Test
+    public void testMax5()
+    {
+       assertEquals(10, Compare.max(-15, 10));
+    }
+}
---
+++ Group1/17020656/lab04/GetMinTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package getmin;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Admin
+ */
+public class GetMinTest {
+    @Test
+    public void testMin1()
+    {
+        assertEquals(2,GetMin.Min(new int[]{2,3,4,5}));
+    }
+    @Test
+    public void testMin2()
+    {
+        assertEquals(-3,GetMin.Min(new int[]{4,5,-3,1,9,0}));
+    }
+    @Test
+    public void testMin3()
+    {
+        assertEquals(-10,GetMin.Min(new int[]{-10,9,-3,1,7,6,10}));
+    }
+    @Test
+    public void testMin4()
+    {
+        assertEquals(5,GetMin.Min(new int[]{6,5,20,100,9,17}));
+    }
+    @Test
+    public void testMin5()
+    {
+        assertEquals(3,GetMin.Min(new int[]{4,5,3,7,9}));
+    }
+}
