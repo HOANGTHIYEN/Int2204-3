0c5e8ab8927456f1bdf44656fdb6e57bd6c807e4
duonghoa190299 <43292103+duonghoa190299@users.noreply.github.com>
2018-10-08 17:59:39 +0700

    tuan5

---
+++ Group1/17020753/lab05/QuaCam.java
@@ -0,0 +1,13 @@
+package tuan5;
+
+
+
+public class QuaCam extends HoaQua{
+
+	public QuaCam(String nguonGoc, String ngayNhap, String soLuong, String giaBan) {
+		super(nguonGoc, ngayNhap, soLuong, giaBan);
+
+	}
+	
+	
+}
