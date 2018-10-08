bb90dbd5e28a5d9cea0d21511145a6aebd2a3e26
DxT9499 <37436272+DxT9499@users.noreply.github.com>
2018-10-08 16:30:40 +0700

    My Commit Message

---
+++ Group2/17021116/lab05/lab5/Circle.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab5;
+
+/**
+ *
+ * @author 123456789
+ */
+public class Circle extends Shape{
+    private double radius = 1.0;
+    private double pi = Math.PI;
+    public Circle(){};
+    public Circle(double radius){
+        this.radius = radius;
+    }
+    public Circle(double radius, String color, boolean filled){
+        super(color,filled);
+        this.radius = radius;
+//        this.color = color;
+//        this.filled = filled;
+    }
+    public double getRadius(){
+        return radius;
+    }
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+    public double getArea(){
+        return pi*radius*radius;
+    }
+    public double getPerimeter(){
+        return 2*pi*radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" + "radius=" + radius + ", pi=" + pi + '}';
+    }
+    
+}
