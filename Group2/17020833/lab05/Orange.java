c57af55f62567580b55ddfd6f800787709f53ea3
NguyenKhanhIE3 <37476894+NguyenKhanhIE3@users.noreply.github.com>
2018-10-09 19:25:13 +0700

    Add files via upload

---
+++ Group2/17020833/lab05/Orange.java
@@ -0,0 +1,20 @@
+package commuio.com;
+
+public class Orange extends  Fruit  {
+
+	public Orange() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Orange(String ten, String nguonGoc, double gia, String ngayNhap, int soLuong) {
+		super(ten, nguonGoc, gia, ngayNhap, soLuong);
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getInfo ()
+	{
+		return "Đây là quả cam ";
+		
+	}
+}
