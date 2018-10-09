125ac7d50086937d2cf7a4ad3e37c75effc9569f
nguyenkhacuong <43382844+nguyenkhacuong@users.noreply.github.com>
2018-10-09 12:44:18 +0700

    z

---
+++ Group1/17020627/lab05/main.java
@@ -0,0 +1,14 @@
+package bai2tuan5;
+public class main {
+
+ 
+    public static void main(String[] args) {
+        Circle tron = new Circle(4.5, " red ", true);
+        System.out.println(tron.toString());
+        Rectangle CN = new Rectangle(3, 4);
+        System.out.println(CN.toString());
+        Square vuong = new Square(5);
+        System.out.println(vuong.toString());
+    }
+    
+}
