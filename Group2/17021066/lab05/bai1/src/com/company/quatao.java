ac713d824150979ce14617084d296e207e22d3eb
toannguyen5699 <toannguyen050699@gmail.com>
2018-10-09 20:34:54 +0700

    my summit

---
+++ Group2/17021066/lab05/bai1/src/com/company/quatao.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class quatao  extends hoaqua{
+    String LoaiTao;
+
+    public String getLoaiTao() {
+        return LoaiTao;
+    }
+
+    public void setLoaiTao(String LoaiTao) {
+        this.LoaiTao = LoaiTao;
+    }
+    public void getinfo() {
+        System.out.println(LoaiTao +"\n");
+    }
+
+}
