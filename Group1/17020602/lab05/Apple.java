9b08b943034348be17cded7e7994437c34cb9a48
DoTuanBac <43380137+DoTuanBac@users.noreply.github.com>
2018-10-09 20:08:44 +0700

    Tuần 5

---
+++ Group1/17020602/lab05/Apple.java
@@ -0,0 +1,44 @@
+
+package week5;
+
+public class Apple extends Fruit{
+   private String price,source,number,day;
+
+    public Apple(String Kind) {
+        super(Kind);
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+ 
+    
+}
---
+++ Group1/17020602/lab05/CamSanh.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week5;
+
+/**
+ *
+ * @author Do Tuan Bac
+ */
+public class CamSanh extends Orange {
+      String price,source,number,day;
+
+    public CamSanh(String Kind) {
+        super(Kind);
+    }
+
+
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+}
