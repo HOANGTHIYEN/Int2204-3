966accd648c1708d432c29acc6b4c5ae18fb6da1
clgt1903 <33454536+clgt1903@users.noreply.github.com>
2018-10-10 17:38:11 +0700

    Delete Apple.java

--- Apple.java
+++
@@ -1,8 +0,0 @@
-
-package Week5;
-
-public class Apple extends Fruit {
-    public Apple(String price, String date, String number, String origin){
-        super(price, date, number, origin);
-    }
-}
