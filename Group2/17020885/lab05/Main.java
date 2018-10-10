31b214121853fa907c5e8706537312ce6109350e
Nguyen Chi Dat <40425312+ChiDatNguyen@users.noreply.github.com>
2018-10-10 23:07:44 +0700

    Update Group2/17020885/lab05/Main.java

--- Group2/17020885/lab05/Main.java
+++ Group2/17020885/lab05/Main.java
@@ -3,41 +3,28 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hoaqua;
+package shape;
 
 /**
  *
  * @author Dell
  */
-public class Main {
+public class main {
     public static void main(String[] args) {
+        circle ht=new circle(1.0);
         
+        System.out.println(ht.toString());
+        System.out.println("Chu vi: "+ht.getPeirmeter()+"\n Dien tich: "+ht.getArea());
+        Restangle hcn=new Restangle();
+        hcn.setLength(3.4);
+        hcn.setWidth(1.4);
+          System.out.println(hcn.toString());
+        System.out.println("Chu vi: "+hcn.getPeirmeter()+"\n Dien tich: "+hcn.getArea());
+        square hv=new square();
+        hv.setWidth(3.4);
         
-        camsanh cs=new camsanh();
-        cs.setTen("Cam sành");
-        cs.setMau("Lục nhạt");
-        cs.setNgaynhap("20/9/2018");
-        cs.setNguongoc("Đồng nai");
-        cs.setGiaban("15000");
-        cs.setSoluong("1000");
-        System.out.println(cs.getInfo());
+         System.out.println("Chu vi: "+hv.getPeirmeter()+"\n Dien tich: "+hv.getArea());
         
-        quaacamphong cp=new quaacamphong();
-        cp.setTen("Cam phong");
-        cp.setMau("Cam");
-        cp.setNgaynhap("20/8/2018");
-        cp.setVi("Ngọt");
-        cp.setSoluong("10000");
-        System.out.println(cp.getInfo());
-        
-        quatao tao=new quatao();
-        tao.setTen("Quả táo");
-        tao.setMau("Tím");
-        tao.setNgaynhap("20/9/2018");
-        tao.setNguongoc("Hòa bình");
-        tao.setGiaban("13000");
-        
-        System.out.println(tao.getInfo());
         
     }
 }
