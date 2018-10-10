f2a1faeb35d6725436976c6651e8f2bd784c4274
103B_Project <37410268+dangbahuy99@users.noreply.github.com>
2018-10-10 19:54:45 +0700

    Add files via upload

---
+++ Group1/17020796/lab05/main.java
@@ -0,0 +1,18 @@
+package bai1;
+
+public class main {
+    public static void main(String[] args) {
+        quaTao apple = new quaTao();
+        apple.show();
+
+        quaCam cam2 = new quaCam();
+        cam2.show();
+
+        camCaoPhong cam = new camCaoPhong();
+        cam.show();
+
+        camSanh cam1 = new camSanh();
+        cam1.show();
+    }
+
+}
