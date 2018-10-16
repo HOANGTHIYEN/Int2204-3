06c35aacac8e3334bb691daa95e608dc59314763
nguyenkhacuong <43382844+nguyenkhacuong@users.noreply.github.com>
2018-10-16 23:32:17 +0700

    ad

---
+++ Group1/17020627/lab06/La6.java
@@ -0,0 +1,14 @@
+
+package la6;
+
+
+public class La6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
