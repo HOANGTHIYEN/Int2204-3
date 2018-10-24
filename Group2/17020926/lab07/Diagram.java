e309193b3c8757756f4eb4fde164c111f76443c7
zenon191 <37409773+zenon191@users.noreply.github.com>
2018-10-24 09:59:47 +0700

    Add files via upload

---
+++ Group2/17020926/lab07/Diagram.java
@@ -0,0 +1,28 @@
+package lab06;
+
+import java.util.ArrayList;
+
+public class Diagram {
+	private ArrayList<Layer> layers;
+	Diagram(){
+		layers = new ArrayList<>();
+	}
+	public void setLayer(Layer layer) {
+		layer.setIndex(layers.size());
+		layers.add(layer);
+	}
+	public Layer getLayer(int index) {
+		return layers.get(index);
+	}
+	public boolean deleteAllCircleInDraw() {
+		for(int i=0;i<layers.size();i++) {
+			for(int j=0;j<layers.get(i).shapes.size();j++) {
+				boolean deleteDone = layers.get(i).deleteAllCircle();
+				if(!deleteDone) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+}
