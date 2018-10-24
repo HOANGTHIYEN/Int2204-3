a86e7dc13c39a8a793ac191f9fe911e41fe997ba
=quang0202 <=quang.dovan0202@gmail.com>
2018-10-24 23:16:02 +0700

    commit lap_07

---
+++ Group2/17020977/lab07/Shape.java
@@ -0,0 +1,24 @@
+package lap_06;
+
+import javafx.scene.paint.Color;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+public abstract class Shape {
+    public XYlocation xy;
+    Color color= Color.BLACK;
+
+    public Shape(XYlocation xy) {
+        this.xy = xy;
+    }
+    
+    public Shape(XYlocation xy, Color color) {
+        this.xy = xy;
+        this.color = color;
+    }        
+    public abstract void moving(XYlocation newLocation);
+}
