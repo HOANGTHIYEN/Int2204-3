bf3d129761fc5da7527d9ccc7a3606e949954af7
doanduyhien203 <doanhien203302@gmail.com>
2018-10-25 13:38:47 +0700

    lap 07

---
+++ Group1/17020729/lab07/week7/Hexagon.java
@@ -0,0 +1,66 @@
+package week7;
+
+public class Hexagon extends Shape{
+
+    public int x1;
+    public int y1;
+    public int x2;
+    public int y2;
+    public int x3;
+    public int y3;
+    public int x4;
+    public int y4;
+    public int x5;
+    public int y5;
+    public int x6;
+    public int y6;
+
+    
+    public int getX1() {
+        return x1;
+    }
+
+    public int getY1() {
+        return y1;
+    }
+
+    public int getX2() {
+        return x2;
+    }
+
+    public int getY2() {
+        return y2;
+    }
+
+    public int getX3() {
+        return x3;
+    }
+
+    public int getY3() {
+        return y3;
+    }
+
+    public int getX4() {
+        return x4;
+    }
+
+    public int getY4() {
+        return y4;
+    }
+
+    public int getX5() {
+        return x5;
+    }
+
+    public int getY5() {
+        return y5;
+    }
+
+    public int getX6() {
+        return x6;
+    }
+
+    public int getY6() {
+        return y6;
+    }
+}
