63fa1284abd608b22a3784a84b94dc674ca9c853
nngocgioi2712 <43361287+nngocgioi2712@users.noreply.github.com>
2018-10-30 23:29:48 +0700

    Add files via upload

---
+++ Group1/17020708/lab08/NullPointer.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab8_c2;
+
+    
+public class NullPointer {
+    public static void main(String args[]) throws NullPointerException {
+        String s = null;
+        System.out.println(s.length());
+        System.out.println("Luong binh thuong...");
+    }
+}
