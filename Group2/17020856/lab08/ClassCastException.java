731aa72d40685d998e8a70e84bb7cfd85ec3e339
nql1211 <37408306+nql1211@users.noreply.github.com>
2018-10-30 11:29:58 +0700

    Add files via upload

---
+++ Group2/17020856/lab08/ClassCastException.java
@@ -0,0 +1,14 @@
+package lab08.bai2;
+
+public class ClassCastException {
+    public static void main (String args[]) throws java.lang.ClassCastException {
+        try{
+            Object obj = new Integer(3);
+            String s = (String) obj;
+        }
+        catch(java.lang.ClassCastException e)
+        {
+            System.err.println(e);
+        }
+    }
+}
