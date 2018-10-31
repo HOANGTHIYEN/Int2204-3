582eb016d22d225d1a4ef14ea18948257b63c77a
clgt1903 <33454536+clgt1903@users.noreply.github.com>
2018-10-31 21:56:57 +0700

    Week8

---
+++ Group1/17020607/lab08/Expression.java
@@ -0,0 +1,13 @@
+package week8;
+
+public abstract class Expression {
+
+    public abstract int evaluate();
+
+    public abstract String toString();
+
+    public Expression() {
+        //Emty
+    }
+
+}
