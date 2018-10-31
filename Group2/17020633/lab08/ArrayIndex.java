e0a796159434b6111b9915bf791d68db951d76b0
diepninh <37438694+diepninh@users.noreply.github.com>
2018-10-31 22:24:43 +0700

    Add files via upload

---
+++ Group2/17020633/lab08/ArrayIndex.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab08.pkg2;
+
+/**
+ *
+ * @author Dell
+ */
+public class ArrayIndex {
+    public ArrayIndex()
+    {
+    }
+   public void array(int [] arr) throws ArrayIndexOutOfBoundsException
+   {   
+       System.out.println(arr[101]);
+   }
+    public static void main(String[] args) {
+        int[] arr = new int [100];
+        try {   new ArrayIndex().array(arr);
+            
+        }
+        catch(ArrayIndexOutOfBoundsException e)
+        {    System.out.println(e);
+        }
+    }
+}
