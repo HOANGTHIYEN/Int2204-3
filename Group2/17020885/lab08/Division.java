b46640f6bc76a348f6cb105b21c12267bd7adee0
tdm0908 <37433554+tdm0908@users.noreply.github.com>
2018-10-31 20:46:35 +0700

    Add files via upload

---
+++ Group2/17020885/lab08/Division.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication30;
+
+/**
+ *
+ * @author dell
+ */
+public class Division extends Expression{
+    Expression ex;
+    Expression ex1;
+    public  Division (Expression ex,Expression ex1) 
+    {   this.ex=ex;
+        this.ex1=ex1;
+    }
+    @Override
+    public int evaluate() {
+       return this.ex.evaluate()/this.ex1.evaluate();
+    }
+
+    @Override
+    public String toString() {
+      return ex + " / " + ex1; 
+    }
+    
+}
+
