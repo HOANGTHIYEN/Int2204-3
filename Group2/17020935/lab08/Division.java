0d99b4fecd47a6e6477fd38012e5bd388073c0b1
trantuanngoc <38138845+trantuanngoc@users.noreply.github.com>
2018-10-31 23:31:38 +0700

    Add files via upload

---
+++ Group2/17020935/lab08/Division.java
@@ -0,0 +1,27 @@
+package lab8;
+
+public class Division extends BinaryExpression {
+    public Division(Expression a, Expression b) {
+        super(a, b);
+    }
+
+    @Override
+    public Expression left() {
+        return super.left();
+    }
+
+    @Override
+    public Expression right() {
+        return super.right();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s/%s", left, right);
+    }
+
+    @Override
+    public int evaluate() {
+        return left.evaluate() / right.evaluate();
+    }
+}
