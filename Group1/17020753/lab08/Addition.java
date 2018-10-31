528d205524dc4f9d5d97441f18a7d59c252f3514
duonghoa190299 <43292103+duonghoa190299@users.noreply.github.com>
2018-10-31 11:01:33 +0700

    lab08

---
+++ Group1/17020753/lab08/Addition.java
@@ -0,0 +1,36 @@
+package lab08;
+
+public class Addition extends BinaryExpression {
+	Expression left, right;
+	
+	public Addition(Expression left, Expression right) {
+		this.left = left;
+		this.right = right;
+	}
+	
+	public Addition() {}
+		
+
+	@Override
+	public Expression left() {
+		return left;
+	}
+
+	@Override
+	public Expression right() {
+		return right;
+	}
+
+	@Override
+	public String toString() {
+		return (left.toString() + " + " + right.toString());
+	}
+
+	@Override
+	public int evaluate() {
+		return (left.evaluate() + right.evaluate());
+	}
+	
+	
+	
+}
