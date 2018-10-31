f61479ce0b088a615d5f3eff37917f8616bcc190
TranVanTu123 <37525938+TranVanTu123@users.noreply.github.com>
2018-10-31 21:27:59 +0700

    Add files via upload

---
+++ Group2/17021098/lab08/Addition.java
@@ -0,0 +1,31 @@
+package lab08.bai1;
+
+public class Addition extends BinaryExpression {
+    Expression left;
+    Expression right;
+
+    public Addition(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public Expression left() {
+        return left;
+    }
+
+    @Override
+    public Expression right() {
+        return right;
+    }
+
+    @Override
+    public String toString() {
+        return (left.toString()+" + "+right.toString());
+    }
+
+    @Override
+    public int evaluate() {
+        return (left.evaluate()  + right.evaluate());
+    }
+}
